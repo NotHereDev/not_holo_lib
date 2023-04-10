af9b1f3fcb0d3e7889fd76e2aa587ae4b2467482
William Richer <william.richer@unova.fr>
2023-04-10 19:05:43 +0200

    update ps script

--- setup_test_server.ps1
+++ setup_test_server.ps1
@@ -1,5 +1,5 @@
 
-$MINECRAFT_VERSION=1.16.5
+$MINECRAFT_VERSION="1.16.5"
 [System.IO.Directory]::CreateDirectory($PSScriptRoot + "/test_server/" + $MINECRAFT_VERSION)
 [System.IO.Directory]::CreateDirectory($PSScriptRoot + "/test_server/build_tools")
 Set-Location ./test_server/build_tools
