402e6f88fd69db41733e4c87ee04ab197ab1c016
RsMan-Dev <wily.richeroffi@gmail.com>
2023-04-10 19:30:34 +0200

    update ps script

--- setup_test_server.ps1
+++ setup_test_server.ps1
@@ -1,7 +1,9 @@
 
+Invoke-WebRequest -z BuildTools.jar -o BuildTools.jar https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar
+
 $MINECRAFT_VERSION="1.16.5"
 [System.IO.Directory]::CreateDirectory($PSScriptRoot + "/test_server/$MINECRAFT_VERSION")
 [System.IO.Directory]::CreateDirectory($PSScriptRoot + "/test_server/build_tools")
 Set-Location ./test_server/build_tools
-java -jar ../../BuildTools.jar --skip-compile --compile-if-changed --rev=$MINECRAFT_VERSION --output-dir="../$MINECRAFT_VERSION"
+java -jar ../../BuildTools.jar --compoile-if-changed --rev"$MINECRAFT_VERSION" --output-dir="../$MINECRAFT_VERSION"
 Set-Location ../..
