1c11d9da3b0bdfadd84bfd9919227b5d1a1aed96
RsMan-Dev <wily.richeroffi@gmail.com>
2023-04-10 17:15:52 +0200

    Initial commit

---
+++ src/main/kotlin/fr/not_here/not_holo_lib/NotHoloLib.kt
@@ -0,0 +1,13 @@
+package fr.not_here.not_holo_lib
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class NotHoloLib : JavaPlugin() {
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
