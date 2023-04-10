167878463597e9d759c8adbbaf7660da7ca3a0e8
RsMan-Dev <wily.richeroffi@gmail.com>
2023-04-10 19:08:53 +0200

    update ps script

--- setup_test_server.ps1
+++ setup_test_server.ps1
@@ -3,5 +3,5 @@ $MINECRAFT_VERSION="1.16.5"
 [System.IO.Directory]::CreateDirectory($PSScriptRoot + "/test_server/$MINECRAFT_VERSION")
 [System.IO.Directory]::CreateDirectory($PSScriptRoot + "/test_server/build_tools")
 Set-Location ./test_server/build_tools
-java -jar ../../BuildTools.jar --skip-compile --compile-if-changed --rev $MINECRAFT_VERSION --output-dir "../$MINECRAFT_VERSION"
+java -jar ../../BuildTools.jar --skip-compile --compile-if-changed --rev=$MINECRAFT_VERSION --output-dir="../$MINECRAFT_VERSION"
 Set-Location ../..
